15e7cb7d374c3fd9a317036a1651ef8b0cdf0ced
Jane2do <107877817+Jane2do@users.noreply.github.com>
2022-08-09 12:10:19 -0500

    mychangesPer

--- src/data.js
+++ src/data.js
@@ -1,9 +1,7 @@
-// estas funciones son de ejemplo
+
+//import data from './data/rickandmorty/rickandmorty.js';
+
 
 export const example = () => {
   return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+}; 
