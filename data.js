f33d8ed911914ee40378eb6df933216ab4587efb
Jane2do <107877817+Jane2do@users.noreply.github.com>
2022-09-06 19:18:19 -0500

    Updates

--- data.js
+++ data.js
@@ -1,6 +1,29 @@
-export const filterGender = (genero, data)=>  data.filter(character => character.gender == genero);
-export const filterSpecies= (especie, data) => data.filter(character => character.species== especie);
-export const filterStatus= (status, data) => data.filter(character => character.status== status);
-export const filterType= (tipo, data) => data.filter(character => character.type== tipo);
-export const filterOrigin= (origen, data) => data.filter(character => character.origin.name== origen);
-export const filterLocation= (ubicacion, data) => data.filter(character => character.location.name== ubicacion);
+export const filterGender = (genero, data) => data.filter(character => character.gender == genero);
+export const filterSpecies = (especie, data) => data.filter(character => character.species == especie);
+export const filterStatus = (status, data) => data.filter(character => character.status == status);
+export const filterType = (tipo, data) => data.filter(character => character.type == tipo);
+export const filterOrigin = (origen, data) => data.filter(character => character.origin.name == origen);
+export const filterLocation = (ubicacion, data) => data.filter(character => character.location.name == ubicacion);
+
+export const alphaOrd = (data) => {
+  return data.sort((a, b) => {
+     if (a.name.toUpperCase() > b.name.toUpperCase()) {
+       return -1;
+     }
+     if (a.name.toUpperCase() < b.name.toUpperCase ()) {
+       return 1;
+     }
+   });
+ }
+ 
+export const searchCharacter =(data, input)=>{
+  let array=[];
+  data.forEach(character =>{
+   let databaseInfo = character.name.toLowerCase();
+   if(databaseInfo.indexOf(input)!== -1){
+   array.push(character)
+   }
+
+  })
+  return array;
+}
--- main.js
+++ main.js
@@ -5,38 +5,16 @@ import { filterType } from './data.js';
 import { filterOrigin } from './data.js';
 import { filterLocation } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
+import {alphaOrd} from './data.js';
+import {searchCharacter} from './data.js';
 console.log(filterSpecies);
 console.log(data.results[0]);
 
 //mostrar data
 const section = document.getElementById('container'); 
-
-data.results.slice(0, 50).forEach(element =>{
-        let html= `
-        <section class="pickles class="flex-boxx"">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-           
-        </div>
-        </section>
-        `;
-       section.innerHTML+=html
-})
-
-let dataCharacters= data.results
-
-//Button to reset All Characters
-
-const allCharacters = document.getElementById('btnAllCharacters'); 
-allCharacters.addEventListener("click", resetCharacters);
-function resetCharacters () {
-  section.innerHTML="";
-  console.log(allCharacters);
-  dataCharacters.forEach(element =>{
+const sliceResult= data.results.slice(0, 100)
+function allCards (allData) {
+  allData.forEach(element =>{
     let html= `
     <section class="pickles class="flex-boxx"">
     <img src=${element.image} class="image">
@@ -51,12 +29,74 @@ function resetCharacters () {
     `;
    section.innerHTML+=html
 })
+}
+allCards(sliceResult);
+
+let dataCharacters= data.results;
+
+//Search Function
+let formulario=document.getElementById("inputText");
+//const boton=document.querySelector(".magGlass");
+//const searchResult=document.getElementById("searchResults");
+
+/*const searchCharacter=()=>{
+  searchResult.innerHTML="";
+  console.log(formulario.value);
+  const texto=formulario.value.toLowerCase();
+  for (let character of dataCharacters) {
+    let nombre=character.name.toLowerCase();
+    if (nombre.indexOf(texto) !== -1) {
+      searchResult.innerHTML += `
+        <li class="list">${character.name} <li>
+      `
+    }
+  }
+    if (searchResult.innerHTML === "") {
+      searchResult.innerHTML += `
+      <li class="list" >Character not found...<li>
+    `
+    }
+}*/
+//boton.addEventListener("click", searchCharacter);
+
+/*function whatEver() {
+  console.log(formulario);
+ // console.log(searchCharacter);
+  console.log(searchCharacter(dataCharacters, formulario.value));
+ // return formulario; 
+
+}*/
+//Alphabetical Order
+
+const orderSelect=document.getElementById("order");
+orderSelect.addEventListener ("change", (e) => {
+  let orderBox= alphaOrd(sliceResult);
+  if(e.target.value === "Z-A"){
+    section.innerHTML="";
+    allCards(orderBox);
+  }
+  if(e.target.value === "A-Z"){
+    section.innerHTML="";
+    allCards(orderBox.reverse())
+
+  }
+
+});
+
+//Button to reset All Characters
+
+const allCharacters = document.getElementById('btnAllCharacters'); 
+allCharacters.addEventListener("click", resetCharacters);
+function resetCharacters () {
+  section.innerHTML="";
+  allCards(dataCharacters)
 gender.value="0";
 species.value="0";
 status.value="0";
 type.value="0";
 origin.value="0";
 location.value="0";
+orderSelect.value="0";
 } 
 
 
@@ -68,61 +108,18 @@ mybutton.addEventListener("click",topFunction)
    document.documentElement.scrollTop = 0;
 }
 
-
-//GENDER --opciones
-/*const busqueda = dataCharacters.reduce((dato, filtro)=>{
-  dato[filtro.species] = ++dato[filtro.species] || 0;
-  return dato; 
-}) */
-
-
 //Filtrado género
 const gender = document.getElementById("gender");
 gender.addEventListener("change", (e)=>{
-     console.log(filterGender(e.target.value, dataCharacters));
     
      if (e.target.value==="0") {
       section.innerHTML=""; 
-        dataCharacters.forEach(element =>{
-       
-          let ejercicio = `
-          
-          <section class="pickles" class="flex-boxx">
-          <img src=${element.image} class="image">
-          <div class="lettersWrap" id="prueba">
-              <div class="name">  ${element.name} </div>
-              <div class="species">Species: ${element.species} </div>
-              <div class="status">Status: ${element.status}</div>
-              <img class="pin" id="logo" src= "img/pinRick&M.png">
-  
-          </div>
-          </section>`
-         section.innerHTML+=ejercicio;
-         
-        })
-       
+        allCards(sliceResult)
       }
       else {
       let typeGender = filterGender(e.target.value, dataCharacters);
       section.innerHTML="";
-      console.log(typeGender);
-      typeGender.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-       
-      })
+      allCards(typeGender)
     }  
     
 species.value="0";
@@ -130,163 +127,124 @@ status.value="0";
 type.value="0";
 origin.value="0";
 location.value="0";
+orderSelect.value="0";
 })
 // filter Species
 const species= document.getElementById("species");
 species.addEventListener("change", (e)=>{
-     console.log(filterSpecies(e.target.value, dataCharacters));
-  
+     
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+     allCards(sliceResult)
+    }
+    else {
       let typeSpecies = filterSpecies(e.target.value, dataCharacters);
       section.innerHTML="";
-      console.log(typeSpecies);
-      typeSpecies.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-gender.value="0";
-status.value="0";
-type.value="0";
-origin.value="0";
-location.value="0";
+      allCards(typeSpecies)
+    }
+    gender.value="0";
+    status.value="0";
+    type.value="0";
+    origin.value="0";
+    location.value="0";
     })
 
     // filter Status
 const status= document.getElementById("status");
 status.addEventListener("change", (e)=>{
-     console.log(filterStatus(e.target.value, dataCharacters));
-  
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+     allCards(sliceResult)
+    }
+    else {
       let typeStatus = filterStatus(e.target.value, dataCharacters);
       section.innerHTML="";
   
       console.log(typeStatus);
-      typeStatus.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-gender.value="0";
-species.value="0";
-type.value="0";
-origin.value="0";
-location.value="0";
+      allCards(typeStatus)
+    }
+    gender.value="0";
+    species.value="0";
+    type.value="0";
+    origin.value="0";
+    location.value="0";
     })
 
-    // filter Type
+// filter Type
 const type= document.getElementById("type");
 type.addEventListener("change", (e)=>{
-     console.log(filterType(e.target.value, dataCharacters));
-  
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+      allCards(sliceResult)
+    }
+    else {
       let type = filterType(e.target.value, dataCharacters);
       section.innerHTML="";
-     
-      console.log(type);
-      type.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-gender.value="0";
-species.value="0";
-status.value="0";
-origin.value="0";
-location.value="0";
+  
+      allCards(type)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  origin.value="0";
+  location.value="0";
     })
 
-    // filter Origin
+ // filter Origin
 const origin= document.getElementById("origin");
 origin.addEventListener("change", (e)=>{
-     console.log(filterOrigin(e.target.value, dataCharacters));
-  
+
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+
+      allCards(sliceResult)
+    }
+    else {     
       let typeOrigin = filterOrigin(e.target.value, dataCharacters);
       section.innerHTML="";
-    
-      console.log(typeOrigin);
-      typeOrigin.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
-
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-gender.value="0";
-species.value="0";
-status.value="0";
-type.value="0";
-location.value="0";
+      allCards(typeOrigin)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  type.value="0";
+  location.value="0";
     })
 
-    // filter Location
+// filter Location
 const location= document.getElementById("location");
 location.addEventListener("change", (e)=>{
-     console.log(filterLocation(e.target.value, dataCharacters));
+  if (e.target.value==="0") {
+    section.innerHTML=""; 
+      allCards(sliceResult)
+    }
+    else {
   
       let typeLocation = filterLocation(e.target.value, dataCharacters);
       section.innerHTML="";
-      /*const content=document.getElementById("content")
-      content.innerHTML="";*/
-      console.log(typeLocation);
-      typeLocation.forEach(element =>{
-       
-        let ejercicio = `
-        
-        <section class="pickles" class="flex-boxx">
-        <img src=${element.image} class="image">
-        <div class="lettersWrap" id="prueba">
-            <div class="name">  ${element.name} </div>
-            <div class="species">Species: ${element.species} </div>
-            <div class="status">Status: ${element.status}</div>
-            <img class="pin" id="logo" src= "img/pinRick&M.png">
 
-        </div>
-        </section>`
-       section.innerHTML+=ejercicio;
-      })
-gender.value="0";
-species.value="0";
-status.value="0";
-type.value="0";
-origin.value="0";
-    })
+      allCards(typeLocation)
+    }
+  gender.value="0";
+  species.value="0";
+  status.value="0";
+  type.value="0";
+  origin.value="0";
+    });
+
+    //Evento busqueda
+formulario.addEventListener("keyup", (e) =>{
+  console.log(e.target.value);
+  let modifiedInput= e.target.value.toLowerCase();
+ if(e.target.value !== ""){
+    section.innerHTML="";
+    let searchResults= searchCharacter(dataCharacters, modifiedInput);
+    
+    console.log(searchCharacter(dataCharacters,modifiedInput));
+    if (searchResults.length===0){
+      section.innerHTML="<p>Character not found, maybe in an unknown dimention!!</p>"
+    } else {
+      allCards(searchResults);
+    } 
+  }else{
+    allCards(sliceResult)}});
